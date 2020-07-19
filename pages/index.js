fd866ba1c6f8f383d67165419ce2d2159cdf6fe6
woohyun <brain@ddingdong.net>
2020-07-19 17:00:33 +0900

    feature: make index.js structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
     (
@@ -18,70 +19,21 @@ export default function Home() {
     return (
         <div className="container">
             <Head>
-                <title>Create Next App</title>
+                <title>Next App With Apollo</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
                 <h1 className="title">
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                    Next App With <span>Apollo</span>
                 </h1>
 
-                <p className="description">
-                    Get started by editing <code>pages/index.js</code>
-                </p>
-
-                <div className="grid">
-                    <a href="https://nextjs.org/docs" className="card">
-                        <h3>Documentation &rarr;</h3>
-                        <p>
-                            Find in-depth information about Next.js features and
-                            API.
-                        </p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className="card">
-                        <h3>Learn &rarr;</h3>
-                        <p>
-                            Learn about Next.js in an interactive course with
-                            quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className="card"
-                    >
-                        <h3>Examples &rarr;</h3>
-                        <p>
-                            Discover and deploy boilerplate example Next.js
-                            projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className="card"
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL
-                            with Vercel.
-                        </p>
-                    </a>
-                </div>
+                <Link href="/users">
+                    <p className="description">
+                        Show all users
+                    </p>
+                </Link>
             </main>
-
-            <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-                </a>
-            </footer>
 
             <style jsx>{`
                 .container {
@@ -102,38 +54,14 @@ export default function Home() {
                     align-items: center;
                 }
 
-                footer {
-                    width: 100%;
-                    height: 100px;
-                    border-top: 1px solid #eaeaea;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                footer img {
-                    margin-left: 0.5rem;
-                }
-
-                footer a {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                a {
-                    color: inherit;
-                    text-decoration: none;
-                }
-
-                .title a {
+                .title span {
                     color: #0070f3;
                     text-decoration: none;
                 }
 
-                .title a:hover,
-                .title a:focus,
-                .title a:active {
+                .title span:hover,
+                .title span:focus,
+                .title span:active {
                     text-decoration: underline;
                 }
 
@@ -153,58 +81,8 @@ export default function Home() {
                     font-size: 1.5rem;
                 }
 
-                code {
-                    background: #fafafa;
-                    border-radius: 5px;
-                    padding: 0.75rem;
-                    font-size: 1.1rem;
-                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-                        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
-                        monospace;
-                }
-
-                .grid {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-wrap: wrap;
-
-                    max-width: 800px;
-                    margin-top: 3rem;
-                }
-
-                .card {
-                    margin: 1rem;
-                    flex-basis: 45%;
-                    padding: 1.5rem;
-                    text-align: left;
-                    color: inherit;
-                    text-decoration: none;
-                    border: 1px solid #eaeaea;
-                    border-radius: 10px;
-                    transition: color 0.15s ease, border-color 0.15s ease;
-                }
-
-                .card:hover,
-                .card:focus,
-                .card:active {
-                    color: #0070f3;
-                    border-color: #0070f3;
-                }
-
-                .card h3 {
-                    margin: 0 0 1rem 0;
-                    font-size: 1.5rem;
-                }
-
-                .card p {
-                    margin: 0;
-                    font-size: 1.25rem;
-                    line-height: 1.5;
-                }
-
-                .logo {
-                    height: 1em;
+                .description:hover {
+                    cursor: pointer;
                 }
 
                 @media (max-width: 600px) {
